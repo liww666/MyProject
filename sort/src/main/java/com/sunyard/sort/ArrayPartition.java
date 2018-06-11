ec3c0c38f5248c747dcf7c46720d0621ea179f87
你çlww <1065034454@qq.comssh-keygen -t rsa -C 1065034454@qq.comgit config --global user.email 1065034454@qq.com>
2018-06-11 22:14:13 +0800

    2018-6-11

---
+++ sort/src/main/java/com/sunyard/sort/ArrayPartition.java
@@ -0,0 +1,31 @@
+package com.sunyard.sort;
+
+/**
+ * Created by Administrator on 2018/6/11.
+ */
+public class ArrayPartition {
+    public static void main(String[] args){
+        int[] arr=new int[10];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=(int)(Math.random()*200);
+        }
+        for(int j:arr){
+            System.out.print(j+",");
+        }
+        System.out.println();
+        int left=-1;
+        int right=arr.length;
+        int pivot=99;
+        while(true){
+            while(left<right&&arr[++left]<pivot);
+            while(right>left&&arr[--right]>pivot);
+            if(left>=right)break;
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+        }
+        for(int j:arr){
+            System.out.print(j + ",");
+        }
+    }
+}
